7efc4d18d3137cb8cbd840f6c3c67ade0c03fd77
chrialge <chrialge@gmail.com>
2024-05-21 17:11:13 +0200

    set header for who authorized

--- resources/views/admin/characters/edit.blade.php
+++ resources/views/admin/characters/edit.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.admin')
 @section('content')
     <div class="container">
         <form action="{{ route('characters.update', $character) }}" method="post">
