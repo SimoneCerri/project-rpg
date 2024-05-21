8de6893df7b646a952ecb477e6fbce2455603ac8
SimoneCerri <simone.cerri1994@gmail.com>
2024-05-21 15:46:55 +0200

    setup auth

--- routes/web2.php
+++ routes/web2.php
@@ -1,9 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Guests\ItemController;
 use App\Http\Controllers\CharacterController;
+use App\Http\Controllers\Guests\ItemController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,10 +14,6 @@ use App\Http\Controllers\CharacterController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/', [ItemController::class, 'home'])->name('home');
 
@@ -34,15 +29,3 @@ Route::get('/items', [ItemController::class, 'index'])->name('guests.items.index
 
 Route::get('/items/{item}', [ItemController::class, 'show'])->name('guests.items.show');
 Route::resource('/characters', CharacterController::class);
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
