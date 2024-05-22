28e611364d3ccc115acddbc45a1756dd72de40f8
chrialge <chrialge@gmail.com>
2024-05-22 18:29:32 +0200

    add link agl'items guest

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,6 +47,10 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{ url('/') }}">{{ __('Home') }}</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('guests.items.index') }}">{{ __('Items') }}</a>
+                        </li>
+
                     </ul>
 
                     <!-- Right Side Of Navbar -->
