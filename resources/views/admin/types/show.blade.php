ee86767eca8c8a2fb7eca21041d156ee2a10c316
chrialge <chrialge@gmail.com>
2024-06-01 17:04:26 +0200

    add form for update method for desc of type, this form is in file types/show.blade.php

--- resources/views/admin/types/show.blade.php
+++ resources/views/admin/types/show.blade.php
@@ -9,21 +9,69 @@
 
         </div>
 
+        <div class="row row-cols-2 py-3">
+
+            {{-- show type --}}
+            <div class="col">
+                <div class="project_info pb-4">
+                    <h3 class=" d-inline">Type Name: </h3>
+                    <span style="font-size: 30px;">{{ $type->name }}</span>
+                </div>
 
 
-        <div class="col">
+                <div class="metadata p-2" style="height: 600px; overflow-y:auto">
+                    <p align="justify">
+                        <strong>Description: </strong>
+                        <br />
+                        {{ $type->desc ? $type->desc : 'N/A' }}
+                    </p>
+                </div>
+            </div>
 
-            <h3 class=" d-inline">Type Name: </h3>
-            <span style="font-size: 30px;">{{ $type->name }}</span>
+            {{-- edit type --}}
+            <div class="col">
+
+                <h2>Edit Type</h2>
+
+                <form action="{{ route('admin.types.update', $type) }}" method="post">
+                    @csrf
+                    @method('PUT')
+
+                    {{-- input name --}}
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Name </label>
+                        <input type="text" class="form-control @error('name') is-invalid @enderror" name="name"
+                            id="name" aria-describedby="nameHelper" placeholder="sword"
+                            value="{{ old('name', $type->name) }}" />
+                        <small id="nameHelper" class="form-text text-muted">Type name of your types</small>
+                        @error('name')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+
+                    {{-- input desc --}}
+                    <div class="mb-3">
+                        <label for="desc" class="form-label">Description</label>
+                        <textarea class="form-control @error('desc') is-invalid @enderror" name="desc" id="desc" rows="10">{{ old('desc', $type->desc) }}</textarea>
+                        @error('desc')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+
+
+
+                    {{-- button that a click post data to update --}}
+                    <button class="btn btn-warning" type="submit">
+                        <i class="fa fa-pencil" aria-hidden="true"></i>
+                        EDIT
+                    </button>
+
+                </form>
+            </div>
         </div>
 
 
-        <div class="metadata">
-            <p>
-                <strong>Description: </strong>
-                {{ $type->desc ? $type->desc : 'N/A' }}
-            </p>
-        </div>
+
     </div>
 
 
