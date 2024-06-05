1b012ec433771f9d4afe9153619386bc7a4fd6d3
chrialge <chrialge@gmail.com>
2024-06-05 17:16:24 +0200

    add header layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,23 +2,32 @@
 
 
 @section('content')
-    <div class="container py-5 text-center">
-        <h1>
+    <div class="container py-5  rounded-3 border border-black bg-white">
+        <h1 class="text-center">
             <strong>
                 Welcome to our RPG-Game's website
             </strong>
         </h1>
-        <div class="row">
-            <div class="col-6">
-                <button type="button" class="btn btn-secondary">
-                    Boolean
-                </button>
-            </div>
-            <div class="col-6">
-                <button type="button" class="btn btn-secondary">
-                    Devs
-                </button>
-            </div>
-        </div>
+        <h3 class="py-5">projects of: </h3>
+        <ul class=" list-unstyled d-flex row-cols-3 flex-wrap">
+            <li>
+                <h4>Francesco Berretta</h4>
+            </li>
+            <li>
+                <h4>Simone Cerri</h4>
+            </li>
+            <li>
+                <h4>Raluca Stefan</h4>
+            </li>
+            <li>
+                <h4>Alessandro Cascone</h4>
+            </li>
+            <li>
+                <h4>Christian Algieri</h4>
+            </li>
+
+        </ul>
+
+    </div>
     </div>
 @endsection
